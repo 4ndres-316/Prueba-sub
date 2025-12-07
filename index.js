766772938eec459183d4750458cc02af2b9cc354
4ndres-316 <andresmelna33@gmail.com>
2025-12-06 22:17:41 -0400

    asciende

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const header = document.querySelector("header");
-const main = document.querySelector("main");
+const cards = document.querySelectorAll(".card");
 
-const bootstrapColors = "bg-primary";
+const bootstrapColors = [
+  "bg-primary",
+  "bg-secondary",
+  "bg-success",
+  "bg-danger",
+  "bg-warning",
+  "bg-info",
+  "bg-light",
+  "bg-dark",
+];
+
+let currentIndex = 0;
+
+const botonBuscar = document.querySelector(".btn.btn-warning");
 
 function cambiarColor(elemento) {
   bootstrapColors.forEach((color) => elemento.classList.remove(color));
@@ -10,9 +22,30 @@ function cambiarColor(elemento) {
   elemento.classList.add(nuevoColor);
 }
 
-document
-  .getElementById("#header")
-  .addEventListener("click", () => cambiarColor(header));
-document
-  .getElementById("#main")
-  .addEventListener("click", () => cambiarColor(main));
+botonBuscar.addEventListener("click", () => {
+  cambiarColor(cards[currentIndex]);
+  currentIndex++;
+  if (currentIndex >= cards.length) {
+    currentIndex = 0;
+  }
+});
+
+cards.forEach((card) => {
+  let zoomInterval;
+  let scale = 1;
+
+  card.addEventListener("mouseenter", () => {
+    zoomInterval = setInterval(() => {
+      scale += 0.01;
+      card.style.transform = `scale(${scale})`;
+      card.style.transition = "transform 0.05s linear";
+    }, 50);
+  });
+
+  card.addEventListener("mouseleave", () => {
+    clearInterval(zoomInterval);
+    scale = 1;
+    card.style.transform = "scale(1)";
+    card.style.transition = "transform 0.3s ease-out";
+  });
+});
